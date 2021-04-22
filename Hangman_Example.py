177b0c63d0624b45d82b3ea4831112c437a64cb1
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-04-22 13:23:57 +0100

    Hangman Example

--- Hangman_Example.py
+++ Hangman_Example.py
@@ -1,6 +1,3 @@
-example = "My phrase is this"
-
-
 def hidden_phrase(word: str):
     hid_phr = ""
     for letter in word:
@@ -11,28 +8,52 @@ def hidden_phrase(word: str):
     return hid_phr
 
 
-def letter_index(letter: str):
-    if example.count(letter) > 1:
-        j = 0
-        for i in range(example.count(letter)):
-            pos_i = example.find(letter, j, -1)
-            j = pos_i + 1
-            return pos_i
-    else:
-        position = example.find(letter)
-        return position
+def letter_index(phrase, letter):
+    for i, ch in enumerate(phrase):
+        if ch == letter:
+            return i
 
 
-def replace_letter(letter: str):
-    new_phr = hidden_phrase(example)[1].replace("_", letter)
-    return
+def replace_letter(phrase, replace, letter, hidden_word):
+    whole_phr = phrase
+    for n in [letter_index(hidden_word, letter)]:
+        new_phr = whole_phr[:n] + letter + whole_phr[n+1:]
+        whole_phr = new_phr
+        return whole_phr
 
+replace_letter('__/____/__/__', )
 
-def check_letter(letter: str):
+def play_a_game():
+    player_1 = input("Whoever is deciding the phrase, please type in your name:  ")
+    player_2 = input("Whoever is guessing, please type in your name:  ")
+    sec_word = input(f"{player_1.capitalize()}, please type in your secret phrase:  ")
+    sec_word_view = hidden_phrase(sec_word)
+    print(sec_word_view)
+    already_guessed = set()
     count = 0
-    if letter in example:
-        letter_index(letter)
+    while count < 8:
+        guess = input(f"{player_2.capitalize()}, please choose a letter to guess:  ")
+        if sec_word_view.strip('/') == sec_word.strip(" "):
+            print(f"{player_2.capitalize()} has won and guessed the word, which is {sec_word}")
+            break
+        elif guess in already_guessed:
+            guess = input(print("You've already guessed this, please choose another letter:  "))
+        else:
+            already_guessed.add(guess)
+            print(already_guessed)
+            if guess in sec_word:
+                sec_word_view = replace_letter(sec_word_view, '_', guess, sec_word)
+                print(sec_word_view)
+            else:
+                count += 1
+                print("This is wrong, you have ", str(8 - count), " more guesses")
+    if count == 8:
+        print(f"{player_1.capitalize()} has won, unfortunately {player_2.capitalize()} didn't guess "
+              f"the word, which was {sec_word}")
+    nxt_game = input(print("Do you want to play again (y/n):  "))
+    if nxt_game.lower() == 'y':
+        play_a_game()
     else:
-        count += 1
+        print("Hope you have a lovely day")
 
-print(replace_letter('y'))
+play_a_game()
