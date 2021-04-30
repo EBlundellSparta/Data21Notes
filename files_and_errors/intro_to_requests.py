9ba52ccf9dc4619917403fe87a11f83b598a3796
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-04-30 11:58:22 +0100

    Up to date

--- files_and_errors/intro_to_requests.py
+++ files_and_errors/intro_to_requests.py
@@ -2,12 +2,13 @@ import requests
 import json
 from pprint import pprint
 
-# post_code_req = requests.get("http://api.postcodes.io/postcodes/se120nb")
+post_code_req = requests.get("http://api.postcodes.io/postcodes/aghodgio")
+
+print(post_code_req.json())
+
+# json_body = json.dumps({"postcodes": ["kwjfgijsfbgsfb"]})
+# headers = {"Content-Type": "application/json"}
 #
-# print(post_code_req.json())
-json_body = json.dumps({"postcodes": ["PR3 0SG", "M45 6GN", "EX16 5BL"]})
-headers = {"Content-Type": "application/json"}
-
-post_multi_req = requests.post("http://api.postcodes.io/postcodes", headers=headers, data=json_body)
-
-pprint(post_multi_req.json())
+# post_multi_req = requests.post("http://api.postcodes.io/postcodes", headers=headers, data=json_body)
+#
+# pprint(post_multi_req.json())
