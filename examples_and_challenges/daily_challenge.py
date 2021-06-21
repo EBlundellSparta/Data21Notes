835177c3ec727b1cd54501170f0b42cf647700d4
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-06-21 16:09:04 +0100

    daily challenge

--- examples_and_challenges/daily_challenge.py
+++ examples_and_challenges/daily_challenge.py
@@ -6,3 +6,45 @@ def problem_1(number_lst, k):
             if i + j == k:
                 result = True
     return result
+
+# The sum of primes below 10 is 2+3+5+7 = 17. Find the sum of all primes below two million
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n < 20:
+        ans = False
+        for i in range (2, n):
+            if n%i == 0:
+                ans = False
+                break
+            else:
+                ans = True
+        return ans
+    elif 20 < n < 100:
+        ans = False
+        for i in range (2, 20):
+            if n%i == 0:
+                ans = False
+                break
+            else:
+                ans = True
+        return ans
+    else:
+        ans = False
+        for i in range(2, int(n**(1/2)) + 1):
+            if n%i == 0:
+                ans = False
+                break
+            else:
+                ans = True
+        return ans
+def prime_list(number):
+    lst = []
+    for i in range(number + 1):
+        if is_prime(i):
+            lst.append(i)
+    return sum(lst)
+
+print(prime_list(2000000))
