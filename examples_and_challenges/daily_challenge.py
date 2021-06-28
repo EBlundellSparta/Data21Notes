17ff689e6649a8c602e46d60516fd211aedd4393
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-06-28 13:53:29 +0100

    daily challenges

--- examples_and_challenges/daily_challenge.py
+++ examples_and_challenges/daily_challenge.py
@@ -70,4 +70,82 @@ def pythag_numbers():
                     if a + b + c == 1000:
                         print(a * b * c)
 
-pythag_numbers()
+# Given an array of numbers, find smallest positive integer NOT in the array!
+def small_pos_int(array: list):
+    for i in range(1, 1000000000000000000000000000):
+        if i not in array:
+            return i 
+            break
+
+# Given following constructor pair function, define car function which returns first integer
+# and cdr which returns second integer
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(pair: tuple):
+    def return_first(a,b):
+        return a
+    return pair(return_first)
+
+def cdr(pair: tuple):
+    def return_last(a,b):
+        return b
+    return pair(return_last)
+
+# Given mapping a=1, b=2,..., z=26 and an encoded message, count number of ways message can be decoded
+# i.e. '111' has 3 possible ways, [aaa, ka and ak]
+
+def decode_message(encoded_message: str):
+    decode = {
+        1: 'a',
+        2: 'b',
+        3: 'c',
+        4: 'd',
+        5: 'e',
+        6: 'f',
+        7: 'g',
+        8: 'h',
+        9: 'i',
+        10: 'j',
+        11: 'k',
+        12: 'l',
+        13: 'm',
+        14: 'n',
+        15: 'o',
+        16: 'p',
+        17: 'q',
+        18: 'r',
+        19: 's',
+        20: 't',
+        21: 'u',
+        22: 'v',
+        23: 'w',
+        24: 'x',
+        25: 'y',
+        26: 'z'
+    }
+    count = 1
+    full_str_lst = []
+    for index in range(len(encoded_message) - 1):
+        sub_str = encoded_message[index] + encoded_message[index + 1]
+        left_str = encoded_message[:index]
+        right_str = encoded_message[index + 2:]
+        if index == 0:
+            print(sub_str + '/' + right_str)
+        elif right_str == '':
+            print(left_str + '/' + sub_str)
+        else:
+            print(left_str + '/' + sub_str + '/' + right_str)
+    #     if int(sub_str_2) in decode.keys():
+    #       count += 1
+    #     if index == 0:
+    #         full_str = sub_str + ' ' + encoded_message[index + 2:]
+    #     else:
+    #         full_str = encoded_message[:index] + ' ' + sub_str + ' ' + encoded_message[index + 2:]
+    #     full_str_lst.append(full_str)
+    # print(full_str_lst)
+    # print(count)
+
+decode_message('11124')
